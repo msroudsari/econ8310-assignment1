dd569dd074dfe542182210833d9616bfab90c7d4
msroudsari <ms.roudsari@gmail.com>
2025-02-15 10:34:33 -0600

    3rd

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,24 @@
 import pandas as pd
 import numpy as np
-import pickle
 from pygam import LinearGAM, s
 
 
 data_train = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 data_test = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv")
 
+
+data_train['Timestamp'] = pd.to_datetime(data_train['Timestamp'])
+data_train['year'] = data_train['Timestamp'].dt.year
+data_train['month'] = data_train['Timestamp'].dt.month
+data_train['day'] = data_train['Timestamp'].dt.weekday
+data_train['hour'] = data_train['Timestamp'].dt.hour
+
+
+data_test['Timestamp'] = pd.to_datetime(data_test['Timestamp'])
+data_test['year'] = data_test['Timestamp'].dt.year
+data_test['month'] = data_test['Timestamp'].dt.month
+data_test['day'] = data_test['Timestamp'].dt.weekday
+data_test['hour'] = data_test['Timestamp'].dt.hour
 
 x_train = data_train[['month','day','hour']].values
 y_train = data_train['trips'].values
